9cff2ab89072b3104e072b80673388ef0d169c91
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-01-10 11:44:45 +0100

    Styling changes

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { quiz } from 'reducers/quiz'
-import { Progress } from 'components/Progress'
-import { Options } from 'components/Options'
-
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { quiz } from 'reducers/quiz';
+import { Progress } from 'components/Progress';
+import { Options } from 'components/Options';
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-  const currentIndex = useSelector((state) => state.quiz.currentQuestionIndex)
-  const quizOver = useSelector((state) => state.quiz.quizOver)
-  const answer = useSelector((state) => state.quiz.answers)
-  const dispatch = useDispatch()
-
+  const question = useSelector(
+    state => state.quiz.questions[state.quiz.currentQuestionIndex]
+  );
+  const currentIndex = useSelector(state => state.quiz.currentQuestionIndex);
+  const quizOver = useSelector(state => state.quiz.quizOver);
+  const answer = useSelector(state => state.quiz.answers);
+  const dispatch = useDispatch();
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   return (
@@ -24,10 +24,16 @@ export const CurrentQuestion = () => {
         <img src={question.image} />
         <Options />
         <div>
-          <button type="button" disabled={answer.length === currentIndex} onClick={() => (dispatch(quiz.actions.goToNextQuestion()))}>Next question</button>
+          <button
+            type="button"
+            disabled={answer.length === currentIndex}
+            onClick={() => dispatch(quiz.actions.goToNextQuestion())}
+          >
+            Next question
+          </button>
         </div>
         <Progress />
       </div>
     )
-  )
-}
+  );
+};
